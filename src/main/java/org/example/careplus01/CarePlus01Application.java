4dda06320674339d29f223c0bf97d98eac44afe7
HamzaBee <hamzabenbrahim6060@gmail.com>
2025-12-14 22:48:58 +0100

    first commit

---
+++ src/main/java/org/example/careplus01/CarePlus01Application.java
@@ -0,0 +1,13 @@
+package org.example.careplus01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarePlus01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarePlus01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/careplus01/CarePlus01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.careplus01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarePlus01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
